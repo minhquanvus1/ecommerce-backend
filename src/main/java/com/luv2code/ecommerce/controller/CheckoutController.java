ac35f001c11856915283084bafc8afa3ff329588
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-13 21:53:09 +0700

    set up POST API for payment-intent path

--- src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -1,9 +1,14 @@
 package com.luv2code.ecommerce.controller;
 
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //@CrossOrigin("http://localhost:4200") no need, because we've set CORS mapping for @RestController in MyAppConfig
@@ -21,5 +26,14 @@ public class CheckoutController {
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
